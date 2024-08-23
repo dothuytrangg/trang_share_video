348972b7d58350c1d2c1216f55e52ab59c14e7cb
sunfl025 <dttrang0205@gmail.com>
2024-08-23 15:28:38 +0700

    trang-2

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <h1>Trang-1</h1>
+        <h1>Trang-2</h1>
       </div>
     </main>
   );
