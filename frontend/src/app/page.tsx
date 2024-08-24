c5f21b2a14aaa536b01c443ae6122b55f5534f6c
Ha Thanh <Hai>
2024-08-24 22:49:00 +0700

    code-push

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -94,26 +94,6 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== "open" 
     "& .MuiDrawer-paper": closedMixin(theme),
   }),
 }));
-function Item(props: BoxProps) {
-  const { sx, ...other } = props;
-  return (
-    <Box
-      sx={{
-        p: 1,
-        m: 1,
-        bgcolor: (theme) => (theme.palette.mode === "dark" ? "#101010" : "grey.100"),
-        color: (theme) => (theme.palette.mode === "dark" ? "grey.300" : "grey.800"),
-        border: "1px solid",
-        borderColor: (theme) => (theme.palette.mode === "dark" ? "grey.800" : "grey.300"),
-        borderRadius: 2,
-        fontSize: "0.875rem",
-        fontWeight: "700",
-        ...sx,
-      }}
-      {...other}
-    />
-  );
-}
 
 export default function MiniDrawer() {
   const theme = useTheme();
