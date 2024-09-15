4fe3a5ff96841a24023c05a7803857b5310a3fde
Ha Thanh <Hai>
2024-09-16 04:43:16 +0700

    g

--- frontend/src/contstants/index.ts
+++ frontend/src/contstants/index.ts
@@ -2,6 +2,7 @@ const _GLOBAL = {
     IS_LOGIN : 'TRUE',
     EN:'en',
     VN:'vn',
+    DEFAULT_LANG:'vn',
     DARK:'dark',
     LIGHT:'light'
 }
--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,11 +1,12 @@
 import createMiddleware from 'next-intl/middleware';
+import { _GLOBAL } from './contstants';
 
 export default createMiddleware({
   // A list of all locales that are supported
   locales: ['en', 'vn'],
  
   // Used when no locale matches
-  defaultLocale: 'vn'
+  defaultLocale: _GLOBAL.DEFAULT_LANG
 });
  
 export const config = {
