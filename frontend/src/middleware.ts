e74f28e3db8dfafec148917c67221a36646680a3
Ha Thanh <Hai>
2024-09-21 05:52:45 +0700

    lastest

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,7 +1,6 @@
 import createMiddleware from "next-intl/middleware";
 import { withAuth } from "next-auth/middleware";
 import { NextRequest, NextResponse } from "next/server";
-import NotPermission from "./app/not-permission";
 
 export const locales = ["en", "vn"] as const;
 
