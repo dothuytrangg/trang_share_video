a976ea6fcf70ab031dafa36da4762cd5d302a8eb
letrantuuyen <20043331.uyen@student.iuh.edu.vn>
2024-11-29 17:04:55 +0700

    checkout

--- frontend/src/components/search/SearchResult.tsx
+++ frontend/src/components/search/SearchResult.tsx
@@ -1,31 +1,37 @@
 'use client';
-import { useState, useEffect } from 'react';
-import { useSearchParams } from 'next/navigation';
-import requestApi from '../../../helpers/api';
-import React from 'react';
 import {
     Button,
     Card,
     CardActions,
     CardContent,
     CardMedia,
-    CircularProgress,
-    Pagination,
-    Stack,
+    Grid,
     Tooltip,
     Typography,
-    Grid, // Sử dụng Grid thay vì Stack
+    Pagination,
+    Stack,
 } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import requestApi from "../../../helpers/api";
+import { useLocale, useTranslations } from "next-intl";
 import { _ENV } from "@/contstants";
+import { useRouter } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 
-const SearchPage = () => {
-    const searchParams = useSearchParams(); // Lấy query từ URL
-    const [query, setQuery] = useState<string>(''); // Lưu từ khóa tìm kiếm
-    const [videos, setVideos] = useState<any[]>([]); // Danh sách video
-    const [loading, setLoading] = useState(false);
+export default function SearchPage({ categoryId }: { categoryId: string }) {
+    const [loading, setLoading] = useState(true);
+    const [videos, setVideos] = useState<any[]>([]); // Dùng để lưu mảng video
     const [error, setError] = useState<string | null>(null);
     const [totalPages, setTotalPages] = useState<number>(1); // Tổng số trang
     const [currentPage, setCurrentPage] = useState<number>(1); // Trang hiện tại
+    const [query, setQuery] = useState<string>(''); // Từ khóa tìm kiếm
+    const locale = useLocale();
+    const t = useTranslations("HomePage");
+    const searchParams = useSearchParams(); // Lấy tham số tìm kiếm từ URL
+    const router = useRouter();
+    const [videoDetails, setVideoDetails] = useState([]); // Lưu API gốc
+    var flag = false;
+
 
     useEffect(() => {
         const searchQuery = searchParams.get('query'); // Lấy từ khóa tìm kiếm từ URL
@@ -54,84 +60,98 @@ const SearchPage = () => {
         }
     };
 
+    const handleOnClick = (videoId: string) => {
+        router.push(`/${locale}/detail/${videoId}?categoryId=${categoryId}`);
+    };
+
+    useEffect(() => {
+        if (!flag) {
+            loadVideoDetails();
+            flag = true;
+        }
+
+
+    }, [categoryId]);
+    const loadVideoDetails = async () => {
+
+        try {
+            const res: any = await requestApi(`video-details/${categoryId}`, "GET");
+            console.log('res', res)
+            if (res.success) {
+                setVideoDetails(res.data);
+                const extractedVideos = res.data.map((detail: any) => detail.video);
+                setVideos(extractedVideos);
+            }
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
     const handlePaginationChange = (event: React.ChangeEvent<unknown>, page: number) => {
         setCurrentPage(page);
         fetchVideos(query, page); // Gọi API cho trang mới
     };
 
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <React.Fragment>
-            {/* Hiển thị khi đang tải */}
-            {loading ? (
-                <Stack justifyContent="center" alignItems="center" sx={{ height: '100vh' }}>
-                    <CircularProgress />
-                    <Typography variant="h6" align="center" sx={{ mt: 2 }}>
-                        Đang tải dữ liệu...
-                    </Typography>
-                </Stack>
-            ) : error ? (
+            {/* Hiển thị nếu có lỗi */}
+            {error ? (
                 <Typography variant="h6" color="error" align="center">
                     {error}
                 </Typography>
             ) : (
-                <>
-                    {/* Hiển thị danh sách video */}
-                    <Grid container spacing={2} justifyContent="flex-start">
-                        {videos.map((video: any) => (
-                            <Grid item xs={12} sm={6} md={3} key={video.id}>
-                                <Card sx={{ maxWidth: '100%' }}>
-                                    <CardMedia
-                                        sx={{ height: 140 }}
-                                        image={`${_ENV.NEXT_URL_RESOURCE}/avatars/${video.thumbnail}`}
-                                        title={video.name}
-                                    />
-                                    <CardContent sx={{ height: 140 }}>
-                                        <Typography gutterBottom variant="h6" component="div" sx={{ height: 30, paddingBottom: 8 }}>
-                                            {video.name.length > 50 ? (
-                                                <Tooltip title={video.name}>
-                                                    <span>{`${video.name.substring(0, 50)}...`}</span>
-                                                </Tooltip>
-                                            ) : (
-                                                video.name
-                                            )}
-                                        </Typography>
-                                        <Typography variant="body2" color="text.secondary">
-                                            {video.description.length > 100 ? (
-                                                <Tooltip title={video.description}>
-                                                    <span>{`${video.description.substring(0, 100)}...`}</span>
-                                                </Tooltip>
-                                            ) : (
-                                                video.description
-                                            )}
-                                        </Typography>
-                                    </CardContent>
-                                    <CardActions>
-                                        <Button sx={{ ml: 1, pr: 1, textTransform: 'none', mt: 2 }} color="inherit" variant="contained" size="small">
-                                            Share
-                                        </Button>
-                                        <Button sx={{ ml: 1, pr: 1, textTransform: 'none', mt: 2 }} color="inherit" variant="contained" size="small">
-                                            Learn More
-                                        </Button>
-                                    </CardActions>
-                                </Card>
-                            </Grid>
-                        ))}
-                    </Grid>
-
-                    {/* Phân trang */}
-                    <Stack spacing={2} alignItems="center" sx={{ mt: 3 }}>
-                        <Pagination
-                            count={totalPages}
-                            page={currentPage}
-                            onChange={handlePaginationChange}
-                            color="primary"
-                            size="large"
-                        />
-                    </Stack>
-                </>
+                <Grid container spacing={2} justifyContent="flex-start">
+                    {/* Hiển thị video */}
+                    {videos.map((video: any) => (
+                        <Grid item xs={12} sm={6} md={3} key={video.id}>
+                            <Card sx={{ maxWidth: '100%' }}>
+                                <CardMedia
+                                    sx={{ height: 140 }}
+                                    image={`${_ENV.NEXT_URL_RESOURCE}/videos/${video.thumbnail}`}
+                                    title={video.name}
+                                />
+                                <CardContent sx={{ height: 140 }}>
+                                    <Typography gutterBottom variant="h6" component="div" sx={{ height: 30, paddingBottom: 8 }}>
+                                        {video.name.length > 50 ? (
+                                            <Tooltip title={video.name}>
+                                                <span>{`${video.name.substring(0, 50)}...`}</span>
+                                            </Tooltip>
+                                        ) :
+                                            <span onClick={() => handleOnClick(video.id)} className="cursor-pointer hover:text-blue-600">{video.name}</span>
+                                        }
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        {video.description.length > 100 ? (
+                                            <Tooltip title={video.description}>
+                                                <span>{`${video.description.substring(0, 100)}...`}</span>
+                                            </Tooltip>
+                                        ) : (
+                                            video.description
+                                        )}
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
             )}
+
+            {/* Phân trang */}
+            <Stack spacing={2} alignItems="center" sx={{ mt: 3 }}>
+                <Pagination
+                    count={totalPages}
+                    page={currentPage}
+                    onChange={handlePaginationChange}
+                    color="primary"
+                    size="large"
+                />
+            </Stack>
         </React.Fragment>
     );
-};
-
-export default SearchPage;
+}
